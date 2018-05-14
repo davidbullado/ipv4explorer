d5e96fa73071ff056b1e2b4d0b30b27cf1663960
David Bullado <david.bullado@gmail.com>
2018-05-14 20:01:10 +0200

    Mobile device issue

--- client/javascripts/main.ts
+++ client/javascripts/main.ts
@@ -1,7 +1,8 @@
 import { IPv4 } from "../../ipv4/index";
 import * as L from "leaflet";
 
-let mymap: L.Map = L.map("mapid").setView([0, 0], 1);/*.setMaxBounds([
+let mymap: L.Map = L.map("mapid").fitWorld();
+/*.setView([0, 0], 1);.setMaxBounds([
     [-90, -180],
     [90, 180]
 ])*/
@@ -61,44 +62,6 @@ function castLPoint(p: IPoint): L.Point {
     return point;
 }
 
-/*
-function poligonizify(ipCIDR: string, label: string, color: string):void {
-
-    const twopart: string[] = ipCIDR.split("/");
-    const ipTopLeft: IPv4 = IPv4.newIPv4FromString(twopart[0]);
-    const ipBotLeft: IPv4 = new IPv4();
-    const ipTopRight: IPv4 = new IPv4();
-    const ipBotRight: IPv4 = new IPv4();
-    let mask: number = Number(twopart[1]);
-    const ipEnd: IPv4 = ipTopLeft.getLastIPMask(mask);
-
-    // thanks https://stackoverflow.com/questions/4460586/javascript-regular-expression-to-check-for-ip-addresses
-    let ipReg: string = "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
-    let fullReg: string = "^" + ipReg + "\." + ipReg + "\." + ipReg + "\." + ipReg + "$";
-
-    if (!(new RegExp(fullReg)).test(twopart[0])) {
-        throw new Error("IP CIDR must be defined as: 0.0.0.0/0, instead we got:'" + ipCIDR + "'");
-    }
-    if (isNaN(mask)) {
-        throw new Error("Mask must be defined as: 0.0.0.0/0, instead we got:'" + ipCIDR +"'");
-    }
-
-    // number of lines
-    let height: number = 1 << ((32 - mask) >> 1);
-    let width: number = height ;
-
-    ipBotLeft.pPoint = { x: ipTopLeft.pPoint.x, y: ipTopLeft.pPoint.y + height };
-    ipTopRight.pPoint = { x: ipTopLeft.pPoint.x + width, y: ipTopLeft.pPoint.y };
-    ipBotRight.pPoint = { x: ipTopLeft.pPoint.x + width, y: ipTopLeft.pPoint.y + height };
-
-    L.polygon([
-        getLatLng(mymap, ipTopLeft.pPoint),
-        getLatLng(mymap, ipBotLeft.pPoint),
-        getLatLng(mymap, ipBotRight.pPoint),
-        getLatLng(mymap, ipTopRight.pPoint)
-    ], { color: color,fillColor: color}).addTo(mymap).bindPopup(label+"<br/>"+ipTopLeft.toString() + " to " + ipEnd.toString());
-}*/
-
 var popup: L.Popup = L.popup();
 
 function onMapClick(e: any):void {
@@ -108,11 +71,6 @@ function onMapClick(e: any):void {
     const mypoint: L.Point = mymap.project(e.latlng, mymap.getZoom());
     const myip: IPv4 = IPv4.newIPv4FromPoint( getAbsolutePoint(mymap, e.latlng) );
 
-   /* popup
-        .setLatLng(e.latlng)
-        .setContent(myip.toString())
-        .openOn(mymap);
-*/
     query(myip.toString(), function (whois) {
         popup
         .setLatLng(e.latlng)
@@ -152,39 +110,3 @@ let myip: IPv4 = IPv4.newIPv4FromString(document.getElementById("ip").innerText)
 
 L.marker(getLatLng(mymap, castLPoint(myip.pPoint), 0.5)).addTo(mymap)
     .bindPopup("You are here.<br/>" + myip).openPopup();
-
-
-/*
-const arrCIDR: {
-    ipr: string;
-    lab: string;
-    c: string;
-}[] = [
-        { ipr: "0.0.0.0/8", lab: "Current network (only valid as source address)", c: "#001f3f" },
-        { ipr: "10.0.0.0/8", lab: "Private network", c: "#0074D9" },
-        { ipr: "100.64.0.0/10", lab: "Shared address space for carrier-grade NAT", c: "#7FDBFF" },
-        { ipr: "127.0.0.0/8", lab: "Loopback", c: "#39CCCC" },
-        { ipr: "169.254.0.0/16", lab: "Link-local", c: "#3D9970" },
-        { ipr: "172.16.0.0/12", lab: "Private network", c: "black" },
-        { ipr: "192.0.0.0/24", lab: "IETF Protocol Assignments", c: "#2ECC40" },
-        { ipr: "192.0.2.0/24", lab: "TEST-NET-1, documentation and examples", c: "gray" },
-        { ipr: "192.88.99.0/24", lab: "IPv6 to IPv4 relay (includes 2002::/16)", c: "#01FF70" },
-        { ipr: "192.168.0.0/16", lab: "Private network", c: "#FFDC00" },
-        { ipr: "198.18.0.0/15", lab: "Network benchmark tests", c: "#FF851B" },
-        { ipr: "198.51.100.0/24", lab: "TEST-NET-2, documentation and examples", c: "gray" },
-        { ipr: "203.0.113.0/24", lab: "TEST-NET-3, documentation and examples", c: "gray" },
-        { ipr: "224.0.0.0/4", lab: "IP multicast (former Class D network)", c: "#FF4136" },
-        { ipr: "240.0.0.0/4", lab: "Reserved (former Class E network)", c: "#85144b" },
-        { ipr: "255.255.255.255/32", lab: "Broadcast", c: "#F012BE" }
-    ];
-
-for (let myRange of arrCIDR) {
-    try {
-        poligonizify(myRange.ipr, myRange.lab, myRange.c);
-    } catch (e) {
-        console.log(myRange.ipr + ": " + e);
-        throw e;
-    }
-}
-
-*/
--- dist/bundle.js
+++ dist/bundle.js
@@ -128,7 +128,8 @@ __webpack_require__(/*! ./main */ "./client/javascripts/main.ts");
 Object.defineProperty(exports, "__esModule", { value: true });
 var index_1 = __webpack_require__(/*! ../../ipv4/index */ "./ipv4/index.ts");
 var L = __webpack_require__(/*! leaflet */ "./node_modules/leaflet/dist/leaflet-src.js");
-var mymap = L.map("mapid").setView([0, 0], 1); /*.setMaxBounds([
+var mymap = L.map("mapid").fitWorld();
+/*.setView([0, 0], 1);.setMaxBounds([
     [-90, -180],
     [90, 180]
 ])*/
@@ -175,52 +176,10 @@ function castLPoint(p) {
     var point = new L.Point(p.x, p.y);
     return point;
 }
-/*
-function poligonizify(ipCIDR: string, label: string, color: string):void {
-
-    const twopart: string[] = ipCIDR.split("/");
-    const ipTopLeft: IPv4 = IPv4.newIPv4FromString(twopart[0]);
-    const ipBotLeft: IPv4 = new IPv4();
-    const ipTopRight: IPv4 = new IPv4();
-    const ipBotRight: IPv4 = new IPv4();
-    let mask: number = Number(twopart[1]);
-    const ipEnd: IPv4 = ipTopLeft.getLastIPMask(mask);
-
-    // thanks https://stackoverflow.com/questions/4460586/javascript-regular-expression-to-check-for-ip-addresses
-    let ipReg: string = "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
-    let fullReg: string = "^" + ipReg + "\." + ipReg + "\." + ipReg + "\." + ipReg + "$";
-
-    if (!(new RegExp(fullReg)).test(twopart[0])) {
-        throw new Error("IP CIDR must be defined as: 0.0.0.0/0, instead we got:'" + ipCIDR + "'");
-    }
-    if (isNaN(mask)) {
-        throw new Error("Mask must be defined as: 0.0.0.0/0, instead we got:'" + ipCIDR +"'");
-    }
-
-    // number of lines
-    let height: number = 1 << ((32 - mask) >> 1);
-    let width: number = height ;
-
-    ipBotLeft.pPoint = { x: ipTopLeft.pPoint.x, y: ipTopLeft.pPoint.y + height };
-    ipTopRight.pPoint = { x: ipTopLeft.pPoint.x + width, y: ipTopLeft.pPoint.y };
-    ipBotRight.pPoint = { x: ipTopLeft.pPoint.x + width, y: ipTopLeft.pPoint.y + height };
-
-    L.polygon([
-        getLatLng(mymap, ipTopLeft.pPoint),
-        getLatLng(mymap, ipBotLeft.pPoint),
-        getLatLng(mymap, ipBotRight.pPoint),
-        getLatLng(mymap, ipTopRight.pPoint)
-    ], { color: color,fillColor: color}).addTo(mymap).bindPopup(label+"<br/>"+ipTopLeft.toString() + " to " + ipEnd.toString());
-}*/
 var popup = L.popup();
 function onMapClick(e) {
     var mypoint = mymap.project(e.latlng, mymap.getZoom());
     var myip = index_1.IPv4.newIPv4FromPoint(getAbsolutePoint(mymap, e.latlng));
-    /* popup
-         .setLatLng(e.latlng)
-         .setContent(myip.toString())
-         .openOn(mymap);
- */
     query(myip.toString(), function (whois) {
         popup
             .setLatLng(e.latlng)
@@ -252,40 +211,6 @@ mymap.on("click", onMapClick);
 var myip = index_1.IPv4.newIPv4FromString(document.getElementById("ip").innerText);
 L.marker(getLatLng(mymap, castLPoint(myip.pPoint), 0.5)).addTo(mymap)
     .bindPopup("You are here.<br/>" + myip).openPopup();
-/*
-const arrCIDR: {
-    ipr: string;
-    lab: string;
-    c: string;
-}[] = [
-        { ipr: "0.0.0.0/8", lab: "Current network (only valid as source address)", c: "#001f3f" },
-        { ipr: "10.0.0.0/8", lab: "Private network", c: "#0074D9" },
-        { ipr: "100.64.0.0/10", lab: "Shared address space for carrier-grade NAT", c: "#7FDBFF" },
-        { ipr: "127.0.0.0/8", lab: "Loopback", c: "#39CCCC" },
-        { ipr: "169.254.0.0/16", lab: "Link-local", c: "#3D9970" },
-        { ipr: "172.16.0.0/12", lab: "Private network", c: "black" },
-        { ipr: "192.0.0.0/24", lab: "IETF Protocol Assignments", c: "#2ECC40" },
-        { ipr: "192.0.2.0/24", lab: "TEST-NET-1, documentation and examples", c: "gray" },
-        { ipr: "192.88.99.0/24", lab: "IPv6 to IPv4 relay (includes 2002::/16)", c: "#01FF70" },
-        { ipr: "192.168.0.0/16", lab: "Private network", c: "#FFDC00" },
-        { ipr: "198.18.0.0/15", lab: "Network benchmark tests", c: "#FF851B" },
-        { ipr: "198.51.100.0/24", lab: "TEST-NET-2, documentation and examples", c: "gray" },
-        { ipr: "203.0.113.0/24", lab: "TEST-NET-3, documentation and examples", c: "gray" },
-        { ipr: "224.0.0.0/4", lab: "IP multicast (former Class D network)", c: "#FF4136" },
-        { ipr: "240.0.0.0/4", lab: "Reserved (former Class E network)", c: "#85144b" },
-        { ipr: "255.255.255.255/32", lab: "Broadcast", c: "#F012BE" }
-    ];
-
-for (let myRange of arrCIDR) {
-    try {
-        poligonizify(myRange.ipr, myRange.lab, myRange.c);
-    } catch (e) {
-        console.log(myRange.ipr + ": " + e);
-        throw e;
-    }
-}
-
-*/ 
 
 
 /***/ }),
@@ -302,6 +227,8 @@ for (let myRange of arrCIDR) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var ipv4_1 = __webpack_require__(/*! ./ipv4 */ "./ipv4/ipv4.ts");
 exports.IPv4 = ipv4_1.default;
+var ipv4_2 = __webpack_require__(/*! ./ipv4 */ "./ipv4/ipv4.ts");
+exports.getIPFromXYZ = ipv4_2.getIPFromXYZ;
 
 
 /***/ }),
@@ -328,6 +255,21 @@ function getIpVal(x, y, z) {
     }
     return ipval;
 }
+function getIPFromXYZ(x, y, z) {
+    var ipval = 0;
+    var point = { x: 0, y: 0 };
+    for (var i = z; i > 0; i--) {
+        point.x = x >> (i - 1) & 1;
+        point.y = y >> (i - 1) & 1;
+        var m = point.x | (point.y << 1);
+        var n = point.x | point.y;
+        ipval += m * Math.pow(2, n * (32 - ((z - i + 1) << 1)));
+    }
+    var strip = (ipval >> 24 & 255) + "." + (ipval >> 16 & 255) + "." + (ipval >> 8 & 255) + "." + (ipval & 255);
+    var myIP = new IPv4(ipval);
+    return myIP;
+}
+exports.getIPFromXYZ = getIPFromXYZ;
 var IPv4 = /** @class */ (function () {
     function IPv4(value) {
         if (value === void 0) { value = 0; }
@@ -431,7 +373,7 @@ exports = module.exports = __webpack_require__(/*! ../../node_modules/css-loader
 
 
 // module
-exports.push([module.i, "html, body {\r\n    margin: 0;\r\n    padding: 0;\r\n    width: 100%;\r\n    height: 100%;\r\n    font: 14px \"Lucida Grande\", Helvetica, Arial, sans-serif;\r\n    background-color: black;\r\n}\r\n\r\na {\r\n    color: #00B7FF;\r\n}\r\n\r\n#mapid {\r\n\twidth: 100%;\r\n\theight: 100%;\r\n\tbackground-color: black;\r\n\tposition: static !important;\r\n}\r\n\r\n#ip {\r\n    display: none;\r\n}\r\n\r\n.whois {\r\n    max-height:200px;\r\n    white-space: pre;\r\n    overflow:scroll;\r\n}", ""]);
+exports.push([module.i, "html, body {\r\n    margin: 0;\r\n    padding: 0;\r\n    font: 14px \"Lucida Grande\", Helvetica, Arial, sans-serif;\r\n    background-color: black;\r\n}\r\n\r\nhtml, body, #mapid {\r\n    height: 100%;\r\n    width: 100vw;\r\n}\r\n\r\na {\r\n    color: #00B7FF;\r\n}\r\n\r\n#mapid {\r\n\tbackground-color: black;\r\n\tposition: static !important;\r\n}\r\n\r\n#ip {\r\n    display: none;\r\n}\r\n\r\n.whois {\r\n    max-height:200px;\r\n    white-space: pre;\r\n    overflow:scroll;\r\n}", ""]);
 
 // exports
 
