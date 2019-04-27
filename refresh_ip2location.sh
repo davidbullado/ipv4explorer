cdcba34db243fab184bbdbfde4b75bdc76947f41
David Bullado <david.bullado@gmail.com>
2019-04-27 19:31:40 +0200

    add script refresh ip2location

---
+++ refresh_ip2location.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#wget -O db1lite.zip "http://www.ip2location.com/download/?token=HJHaZKmCNPer9rN3D7xhxyqTbKS9IB8plPzkzWXnLbOVCBZfJwxh5YagI2qrEyQY&file=DB1LITE"
+#unzip db1lite.zip
+
+#wget -O DBASNLITE.zip "http://www.ip2location.com/download/?token=HJHaZKmCNPer9rN3D7xhxyqTbKS9IB8plPzkzWXnLbOVCBZfJwxh5YagI2qrEyQY&file=DBASNLITE"
+#unzip DBASNLITE.zip
