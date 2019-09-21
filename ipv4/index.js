c49036385276317429add6ca8e2e9f3df20adb5a
David Bullado <david.bullado@gmail.com>
2019-09-21 21:40:05 +0000

    Repo delete compiled files

--- ipv4/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ipv4_1 = require("./ipv4");
-exports.IPv4 = ipv4_1.default;
-var ipv4_2 = require("./ipv4");
-exports.getIPFromXYZ = ipv4_2.getIPFromXYZ;
-//# sourceMappingURL=index.js.map
