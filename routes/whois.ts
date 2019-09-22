89d0824e1756933ab2b28eadd71fd8ab17f8543d
David Bullado <david.bullado@gmail.com>
2019-09-22 16:10:41 +0000

    whois: upgrade to whois command

--- routes/whois.ts
+++ routes/whois.ts
@@ -2,6 +2,7 @@ import express = require("express");
 import path = require("path");
 import whois = require('whois');
 import dns = require('dns');
+import exec = require('child_process');
 
 const router: express.Router = express.Router();
 
@@ -14,10 +15,17 @@ router.get("/:ip", (req: express.Request, res: express.Response) => {
     let nsReg: string = "^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+$";
 
     if ((new RegExp(fullReg)).test(req.params.ip)) {
+        let command = "whois "+req.params.ip;
+        exec.exec(command, function(error, stdout, stderr){ 
+            res.type("text");
+            res.send(stdout);
+        });
+        /*
         whois.lookup(req.params.ip, function(err, data) {
             res.type("text");
             res.send(data);
         });
+        */
     } else if ((new RegExp(nsReg)).test(req.params.ip)) {
         dns.lookup(req.params.ip, function(err, data) {
             whois.lookup(data, function(err, data) {
