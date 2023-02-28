2bc3b7afadea0a93f3c1cd493d4cde302a77a3f8
David Bullado <david.bullado@goldenbees.fr>
2023-02-28 20:41:28 +0100

    ipv4: add newIPv4FromRange

--- ipv4/ipv4.test.ts
+++ ipv4/ipv4.test.ts
@@ -1,4 +1,4 @@
-import { getIPFromXYZ } from "./ipv4";
+import { getIPFromXYZ, IPv4 } from "./ipv4";
 import { expect } from 'chai';
 import 'mocha';
 
@@ -11,5 +11,17 @@ describe("Get IP from x, y, z coordinates",function() {
     it("should return null when out of bounds", function() {
         const result = getIPFromXYZ(1,0,0) ;
         expect(result).null;
+    });
+});describe("Range",function() {
+    it("should parse an IP Range", function() {
+        const result = IPv4.newIPv4FromRange('1.0.0.0/16') ;
+        expect(result.length).to.equal(2);
+        expect(result[0].pString).to.equal("1.0.0.0");
+        expect(result[1].pString).to.equal("1.0.255.255");
+    });    it("should parse an IP Range", function() {
+        const result = IPv4.newIPv4FromRange('1.0.0.0/9') ;
+        expect(result.length).to.equal(2);
+        expect(result[0].pString).to.equal("1.0.0.0");
+        expect(result[1].pString).to.equal("1.0.1.255");
     });
 });
